797752e3e30146b34183303e1908066d773b39f1
luis <luis.moreno@exatec.tec.mx>
2023-04-20 11:33:57 -0600

    Changed data type of  to

--- feature_setup.py
+++ feature_setup.py
@@ -2,7 +2,7 @@
 col_to_dtypes = {
     "lei":"str",
     "state_code":"category",
-    "county_code":"int32",
+    "county_code":"category",
     "conforming_loan_limit":"category",
     "action_taken":"category",
     "purchaser_type":"category",
