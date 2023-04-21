562875fb922d1cf813dd840ea99a84ae50fc82a1
luis <luis.moreno@exatec.tec.mx>
2023-04-21 11:20:52 -0600

    Changed county_code data type to categorical

--- feature_setup.py
+++ feature_setup.py
@@ -2,7 +2,7 @@
 col_to_dtypes = {
     "lei":"str",
     "state_code":"category",
-    "county_code":"float32",
+    "county_code":"category",
     "conforming_loan_limit":"category",
     "action_taken":"category",
     "purchaser_type":"category",
